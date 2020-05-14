599117d654e300b31fb7ada3253c8205a9ef1b36
mrclasse <natjageni@gmail.com>
2020-05-14 20:12:09 -0300

    changed settings.py

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -15,12 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+from .secrets import *
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4sdmp!m2twbk%=^p&z)-huw3(8rg%)jd1fwksqfb_uh2qmrs4e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
