8ae4765349b4f27e9f678d03873a355795b45b72
mrclasse <natjageni@gmail.com>
2020-05-14 19:57:45 -0300

    adding inheritance template

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+  {
+    'author': 'John Snow',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': 'August 27, 2081'
+  },
+  {
+    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'August 28, 2081'
+  },
+]
+
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return render(request, 'blog/about.html')
+  return render(request, 'blog/about.html',{'title': 'About'})
