9c2e9664d92b235890ca080a723df20c5436be7a
mrclasse <natjageni@gmail.com>
2020-05-14 23:44:35 -0300

    added a few posts and tested the interaction with db

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# we import a timezone method to post the date
+from django.utils import timezone
+# we import the user model from django
+from django.contrib.auth.models import User
 
-# Create your models here.
+# we create a new Post model
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  date_posted = models.DateTimeField(default=timezone.now)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'John Snow',
-    'title': 'Blog Post 1',
-    'content': 'First post content',
-    'date_posted': 'August 27, 2081'
-  },
-  {
-    'author': 'Jane Doe',
-    'title': 'Blog Post 2',
-    'content': 'Second post content',
-    'date_posted': 'August 28, 2081'
-  },
-]
+from .models import Post
 
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'blog/home.html', context)
 
