9aea222ff1a209a0aaa94ba452f2290d1bae536c
mrclasse <natjageni@gmail.com>
2020-05-19 04:55:32 -0300

    added delete update and create post logic

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,13 +3,19 @@ from django.db import models
 from django.utils import timezone
 # we import the user model from django
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # we create a new Post model
-class Post(models.Model):
-  title = models.CharField(max_length=100)
-  content = models.TextField()
-  date_posted = models.DateTimeField(default=timezone.now)
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
-# we import the view
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+from django.urls import path
 
 # there we store our urls
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
